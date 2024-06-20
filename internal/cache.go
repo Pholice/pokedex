ba9ea5a42c4fb884efe313095f867d709ee26bd2
Pholice <excelforged@gmail.com>
2024-06-20 19:47:57 -0400

    Added caching logic

---
+++ internal/cache.go
@@ -0,0 +1,62 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mu    *sync.Mutex
+}
+
+func NewCachce(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+	}
+	go c.reapLoop(interval)
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = cacheEntry{
+		val:       val,
+		createdAt: time.Now().UTC(),
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, ok := c.cache[key]
+	if !ok {
+		return nil, false
+	}
+	val := entry.val
+	return val, true
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(interval)
+	}
+}
+
+func (c *Cache) reap(interval time.Duration) {
+	timeLimit := time.Now().UTC().Add(-interval)
+	for k, v := range c.cache {
+		if v.createdAt.Before(timeLimit) {
+			delete(c.cache, k)
+		}
+	}
+}
